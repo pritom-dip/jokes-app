99205713b485f006bd343ba6be914a67fe81a220
Pritom-Recruit <pritomdip010@gmail.com>
2022-10-06 23:29:37 +0600

    Update ui

--- src/lib/components/Navbar/index.js
+++ src/lib/components/Navbar/index.js
@@ -1,21 +1,26 @@
 import styles from "./Navbar.module.scss";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import DownriangleIcon from "../../assets/images/downTriangle.png";
+import UserIcon from "../../assets/images/user.png";
 
 const menus = [
   {
     name: "Menu 1",
     link: "#",
     submenu: [],
+    icon: "",
   },
   {
     name: "Menu 2",
     link: "#",
     submenu: [],
+    icon: "",
   },
   {
     name: "Menu 3",
     link: "#",
+    icon: UserIcon,
     submenu: [
       {
         name: "Submenu 1",
@@ -39,7 +44,15 @@ const Navbar = () => {
           {menus?.map((menu) => (
             <Link key={menu.name} to={menu.link}>
               <li className={classNames(styles.item)}>
-                <div>{menu?.name}</div>
+                <div className={classNames(styles.heading, styles.flexCenter)}>
+                  {menu?.icon && (
+                    <img className={styles.iconLeft} src={menu.icon} />
+                  )}
+                  <span>{menu?.name}</span>
+                  {menu.submenu.length > 0 && (
+                    <img className={styles.iconRight} src={DownriangleIcon} />
+                  )}
+                </div>
                 {menu.submenu.length > 0 && (
                   <ul className={classNames(styles.submenu)}>
                     {menu.submenu.map((sub) => (
